8d4d34404c1da7ef92ca0e7782677d1344cf2ad3
Atatanoff <atatanoff@ya.ru>
2022-12-15 18:53:36 +0400

    Плюсы наследования

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,6 @@
 from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException # в начале файла
+from selenium.common.exceptions import NoAlertPresentException
+from .locators import BasePageLocators
 import math
 from time import sleep
 
@@ -10,6 +11,13 @@ class BasePage:
         self.url = url
         self.browser.implicitly_wait(timeout)
     
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
     def open(self):
         self.browser.get(self.url)
     
--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,7 @@ from .pages.product_page import ProductPage
 import pytest
 
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+'''@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
@@ -11,12 +11,14 @@ import pytest
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser,link):
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])'''
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
-    page.should_be_add_to_cart_button()
-    page.add_product_to_cart()
-    page.should_be_success_message()
-    page.should_be_success_price()
+    #page.should_be_add_to_cart_button()
+    #page.add_product_to_cart()
+    #page.should_be_success_message()
+    #page.should_be_success_price()
+    page.should_be_login_link()
+    page.go_to_login_page()
